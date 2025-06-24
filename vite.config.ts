56ff072359a9681f8b7bc503569a7dc89783c2f9
Matthew McKeown <matthewisawzome@gmail.com>
2025-06-24 22:39:37 +1000

    added uuid versioning for fun

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import path from "path";
 import { metablock } from "vite-plugin-userscript";
+import { v4 as uuidv4 } from "uuid";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const isDev = mode !== "production";
   return {
-    plugins: [preact(), !isDev && metablock()].filter(Boolean),
+    plugins: [
+      preact(),
+      !isDev &&
+        metablock({
+          file: "./metablock.json",
+          override: {
+            version: uuidv4(),
+          },
+        }),
+    ].filter(Boolean),
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
